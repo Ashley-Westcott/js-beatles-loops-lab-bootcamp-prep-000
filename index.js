56f381b33924b10670770ee02444bc7e65640207
algebraic-torrent-6062 <ashley.e.westcott@gmail.com>
2019-01-15 07:35:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,12 @@ function johnLennonFacts(facts){
   }
 return newArrayTwo
 }
+
+function iLoveTheBeatles(num){
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
